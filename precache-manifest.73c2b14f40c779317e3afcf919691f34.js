889646d6f40fe014e9483f51d7019a6b85f83705
Nefise Karacam <nefise.cekdemir@gmail.com>
2021-05-23 23:46:02 +0700

    Updates

---
+++ precache-manifest.73c2b14f40c779317e3afcf919691f34.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4c066326353f09ab767fd64cfc15ee08",
+    "url": "/ravenous/index.html"
+  },
+  {
+    "revision": "4b3865e9171284f82496",
+    "url": "/ravenous/static/css/main.32937cf8.chunk.css"
+  },
+  {
+    "revision": "5c5b2c756b445921606f",
+    "url": "/ravenous/static/js/2.538f8d68.chunk.js"
+  },
+  {
+    "revision": "4b3865e9171284f82496",
+    "url": "/ravenous/static/js/main.2ca6ddb3.chunk.js"
+  },
+  {
+    "revision": "1444c97d4f0ed3607f27",
+    "url": "/ravenous/static/js/runtime~main.f8ce170b.js"
+  },
+  {
+    "revision": "902df4c501596c42571749648e081c71",
+    "url": "/ravenous/static/media/background_search_desktop.902df4c5.jpg"
+  },
+  {
+    "revision": "5d0905e66c87b168792e1cf920c613ee",
+    "url": "/ravenous/static/media/background_search_mobile.5d0905e6.jpg"
+  }
+]);
